f4f64a54da757210a57039ccc5fa826afe8422d7
brightmanforever022 <brightmanforever@outlook.com>
2020-12-29 01:33:20 +0800

    make data structure for settings

---
+++ src/data/featuredCollection.js
@@ -0,0 +1,34 @@
+export const featuredCollectionData = [
+  {
+    title: 'Rose Bear',
+    handle: '/collections/rose-bear'
+  },
+  {
+    title: 'Galaxy Rose Collection',
+    handle: '/collections/galaxy'
+  },
+  {
+    title: 'Suede Collection',
+    handle: '/collections/persuede'
+  },
+  {
+    title: 'Marbleous Collection',
+    handle: '/collections/marbleous'
+  },
+  {
+    title: 'The Leather Collection',
+    handle: '/collections/leather'
+  },
+  {
+    title: 'The Bloom Box',
+    handle: '/collections/bloom-box'
+  },
+  {
+    title: 'Tie Dye Collection',
+    handle: '/collections/tie-dye'
+  },
+  {
+    title: '24K Gold Dipped Roses',
+    handle: '/collections/24k-gold-dipped-roses'
+  }
+]
--- src/data/homepage.js
+++ src/data/homepage.js
@@ -1,32 +1,47 @@
 export const homepageData = {
-    footerMenu: [
+    heroImage: {
+        desktopImage: '',
+        mobileImage: ''
+    },
+    imageSections: [
         {
-            title: 'Careers',
-            handle: '/pages/careers'
+            title: 'ROSE BEAR',
+            description: 'A decorative teddy bear handmade of roses that last forever.',
+            imageUrl: '//cdn.shopify.com/s/files/1/0157/4420/4900/files/Rose_bears_ac1ba3a8-be9b-4f99-850f-5c3d687363ca_1100x.png',
+            shopLink: '/collections/rose-bear'
         },
         {
-            title: 'Contact Us',
-            handle: '/pages/contact-us'
+            title: '24K GOLD DIPPED ROSE',
+            description: 'Real rose picked at the height of its beauty preserved in 24-karat gold.',
+            imageUrl: '//cdn.shopify.com/s/files/1/0157/4420/4900/files/24K_Gold_Dipped_Rose_1100x.png',
+            shopLink: '/collections/24k-gold-dipped-roses'
         },
         {
-            title: 'Contact Us',
-            handle: '/pages/contact-us'
+            title: 'PERSUEDE COLLECTION',
+            description: 'Be persuasive with our exquisite suede rose box.',
+            imageUrl: '//cdn.shopify.com/s/files/1/0157/4420/4900/files/Suede_White_Round_Large_Red_1100x.png',
+            shopLink: '/collections/persuede'
         },
         {
-            title: 'Contact Us',
-            handle: '/pages/contact-us'
+            title: 'MARBLEOUS COLLECTION',
+            description: 'Bring elegance to your home with our beautiful marble rose box.',
+            imageUrl: '//cdn.shopify.com/s/files/1/0157/4420/4900/files/Marble_Black_Round_Large_White_1100x.png',
+            shopLink: '/collections/marbleous'
         },
         {
-            title: 'Contact Us',
-            handle: '/pages/contact-us'
+            title: 'LEATHER COLLECTION',
+            description: 'Send beautiful long lasting roses in our gorgeous cruelty-free leather.',
+            imageUrl: '//cdn.shopify.com/s/files/1/0157/4420/4900/files/Leather_Black_Round_Large_Red_1100x.png',
+            shopLink: '/collections/leather'
         },
         {
-            title: 'Contact Us',
-            handle: '/pages/contact-us'
-        },
-        {
-            title: 'Contact Us',
-            handle: '/pages/contact-us'
+            title: 'THE BLOOM BOX',
+            description: 'Featuring a hidden drawer to store jewelry, chocolates or accessories, our Bloom Box collection is made with real roses that last up to 5 years.',
+            imageUrl: '//cdn.shopify.com/s/files/1/0157/4420/4900/files/30C131AB-9250-4B7F-9F46-570DF5A25642_1100x.png',
+            shopLink: '/collections/bloom-box'
         }
+    ],
+    blogSection: [
+        
     ]
 }
