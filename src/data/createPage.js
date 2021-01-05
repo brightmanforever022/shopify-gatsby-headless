d64df1fadf46789d876b647128090375a7e61200
brightmanforever022 <brightmanforever@outlook.com>
2021-01-05 08:30:06 +0800

    create page style

---
+++ src/data/createPage.js
@@ -0,0 +1,83 @@
+export const createPageData = {
+    customiserBanner: {
+        title: 'CUSTOMIZE YOUR DOSE OF ROSES',
+        contents: "Choose arrangement style, box material, rose colors and personalize it with letters, numbers or symbols. All handmade with real roses that last up to five years.",
+        buttonText: 'GET STARTED',
+        desktopImageItems: [
+            {
+                image1: '//cdn.shopify.com/s/files/1/0157/4420/4900/files/Image011_c4e5ee80-375a-4309-95b5-3aed4f6fb0f7_600x.png',
+                image2: '//cdn.shopify.com/s/files/1/0157/4420/4900/files/Image05_56846c48-1cfb-4b04-bf36-8dbd36a331bb_600x.png',
+            },
+            {
+                image1: '//cdn.shopify.com/s/files/1/0157/4420/4900/files/Black_Suede_Large_Round_Red_Roses_600x.jpg',
+                image2: '//cdn.shopify.com/s/files/1/0157/4420/4900/files/Image03_b6558d2b-1ad4-46fb-a495-195fb2e327e9_600x.png'
+            },
+            {
+                image1: '//cdn.shopify.com/s/files/1/0157/4420/4900/files/CIN09842_copy_1_600x.jpg',
+                image2: '//cdn.shopify.com/shopifycloud/shopify/assets/no-image-2048-5e88c1b20e087fb7bbe9a3771824e743c244f437e4f8ba93bbf7b11b53f7824c_600x.gif'
+            },
+            {
+                image1: '//cdn.shopify.com/s/files/1/0157/4420/4900/files/CIN00286_copy_1_600x.jpg',
+                image2: '//cdn.shopify.com/shopifycloud/shopify/assets/no-image-2048-5e88c1b20e087fb7bbe9a3771824e743c244f437e4f8ba93bbf7b11b53f7824c_600x.gif'
+            },
+            {
+                image1: '//cdn.shopify.com/s/files/1/0157/4420/4900/files/Gender_Reveal_600x.jpg',
+                image2: '//cdn.shopify.com/s/files/1/0157/4420/4900/files/Image04_c9d6abed-ef4e-4e66-bd63-58b38659c7a8_600x.png'
+            },
+            {
+                image1: '//cdn.shopify.com/s/files/1/0157/4420/4900/files/Attentive_600x.jpg',
+                image2: '//cdn.shopify.com/s/files/1/0157/4420/4900/files/Image08_569994d2-171e-47e9-942d-77e06a694016_600x.png'
+            }
+        ],
+        mobileImageItems: [
+            {
+                image1: '//cdn.shopify.com/s/files/1/0157/4420/4900/files/Image05_56846c48-1cfb-4b04-bf36-8dbd36a331bb_600x.png',
+                image2: '//cdn.shopify.com/s/files/1/0157/4420/4900/files/Gender_Reveal_600x.jpg'
+            },
+            {
+                image1: '//cdn.shopify.com/s/files/1/0157/4420/4900/files/CIN00286_copy_1_600x.jpg',
+                image2: '//cdn.shopify.com/s/files/1/0157/4420/4900/files/CIN09842_copy_1_600x.jpg'
+            },
+            {
+                image1: '//cdn.shopify.com/s/files/1/0157/4420/4900/files/Black_Suede_Large_Round_Red_Roses_600x.jpg',
+                image2: '//cdn.shopify.com/s/files/1/0157/4420/4900/files/Attentive_600x.jpg'
+            }
+        ]
+    },
+    items: [
+        {
+            title: 'ROSE BEAR',
+            url: '/product/rose_bear',
+            imageUrl: '//cdn.shopify.com/s/files/1/0157/4420/4900/products/Medium_Pink_Rose_Bear_x190.jpg?v=1602470960',
+            variants: [
+                {
+                    type: 'Size',
+                    value: 'Medium (14 Inch)'
+                }
+            ],
+            properties: [],
+            price: '79.95',
+            quantity: '3',
+            totalPrice: '239.85'
+        },
+        {
+            title: 'Medium Round Flat',
+            url: '/product/medium_round_flat',
+            imageUrl: '//cdn.shopify.com/s/files/1/0157/4420/4900/products/MediumRoundFlat_x190.png?v=1605088780',
+            variants: [
+                {
+                    type: 'Box',
+                    value: 'White Suede'
+                },
+                {
+                    type: 'Style',
+                    value: 'Solid'
+                }
+            ],
+            properties: [],
+            price: '199.00',
+            quantity: '1',
+            totalPrice: '199.00'
+        }
+    ]
+}
