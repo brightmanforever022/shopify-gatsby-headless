ec5d40e7bf37e3e6e146da3cdbabf3e36f86f880
brightmanforever022 <brightmanforever@outlook.com>
2021-01-13 10:51:17 -0800

    pages build

--- src/data/faq.js
+++ src/data/faq.js
@@ -10,9 +10,11 @@ export const faqPageData = {
             question: 'Where did Dose of Roses come from?',
             answer: [
                 {
+                    type: 'p',
                     content: 'The brand was founded by Joseph Ayoub, founder of some of the most well-known digital brands, and Julian Wilson, a former executive of the #1 most-searched fashion brand. Founded on January 6, 2019, Joseph and Julian were looking to start a new brand together and Valentine’s Day was right around the corner. Roses came to mind as they are the symbol of love and what most people give to their loved ones. Being different from the rest, they began selling this unique gift called the Rose Bear, a bear covered in synthetic roses that last a lifetime. After selling out completely in a matter of weeks, they started roaming through the stores of Los Angeles to find Rose Bears but weren’t able to find it as it was a relatively new product that wasn’t available in stores. Having no luck, they started looking for other gifts and that’s where they came across preserved rose arrangements. They had bought the entire inventory of multiple stores and had sold out of those in a matter of days. This is where the idea of gifting a unique luxury experience that isn’t the traditional bouquet of roses that only last a few days was born.'
                 },
                 {
+                    type: 'p',
                     content: 'Today, you can create hundreds of different arrangements featuring unique shapes, colors, and sizes. Each arrangement contains anywhere from one to more than 150 Timeless Roses, and with proper care, your arrangement can last up to five years'
                 }
             ]
@@ -21,6 +23,7 @@ export const faqPageData = {
             question: 'What are Timeless Roses?',
             answer: [
                 {
+                    type: 'p',
                     content: 'The Timeless Roses used at Dose of Roses® come from the lush, vibrant rose fields of Ecuador. Each rose is carefully cultivated until it reaches full bloom. Each rose is treated with a formula made from non-toxic and non-allergenic wax, as well as natural oils. This formula is similar to silica and effectively dehydrates the roses to help preserve them. At the completion of the dehydration process, the roses soak in dye to give them the bright and vibrant colors you’ve come to expect from our gorgeous Timeless Roses. Once the roses are at their peak, they are picked and shipped to our facility in Los Angeles.'
                 }
             ]
@@ -29,9 +32,11 @@ export const faqPageData = {
             question: 'Do Timeless Roses really last more than a year?',
             answer: [
                 {
+                    type: 'p',
                     content: 'Utilizing the process described above, our Timeless Roses will last up to five years or more with proper care, allowing you to enjoy their beauty month after month. With that in mind, beauty and elegance aren’t the only things people love about roses. They also love the smell!'
                 },
                 {
+                    type: 'p',
                     content: 'The smell of roses is a wildly popular scent, whether it’s used in candles, perfumes, or the flowers themselves. With our Timeless Roses, you not only have the benefit of enjoying these roses for more than a year, but you can also enjoy the lovely scent of roses longer than you would with traditional roses.'
                 }
             ]
@@ -40,9 +45,11 @@ export const faqPageData = {
             question: 'How do I care for my Timeless Roses?',
             answer: [
                 {
+                    type: 'p',
                     content: 'Dose of Roses® Timeless Roses, need minimal care. The dehydration process ensures that our roses are preserved at peak beauty, and water is not needed to keep them that way. In fact, if you want your roses to continue looking their best, it is important that you do not water them, and make sure you keep them contained in the box you received them in.'
                 },
                 {
+                    type: 'p',
                     content: 'Make sure to keep your roses in a room-temperature environment, away from direct sunlight, and in an area where they are notat risk of being crushed. If you notice that your roses are looking a little dusty after a few months, then you can remove the dust with a little light dusting using a very soft and light-weight duster.'
                 }
             ]
@@ -51,6 +58,7 @@ export const faqPageData = {
             question: 'Can I take the roses out of the box?',
             answer: [
                 {
+                    type: 'p',
                     content: 'No. For maximum preservation, we advise customers to not take the roses out of the box. The roses are nice and cozy and precisely placed within the box. Removing the roses will cause damage and ruin the arrangement.'
                 }
             ]
@@ -59,9 +67,11 @@ export const faqPageData = {
             question: 'Do you accept returns?',
             answer: [
                 {
+                    type: 'p',
                     content: 'Unfortunately, our arrangements of Timeless Roses from Dose of Roses® are not eligible for returns. If you have ordered an arrangement of Timeless Roses for same-day delivery, those orders cannot be canceled under any circumstances.'
                 },
                 {
+                    type: 'p',
                     content: 'Our hope, at Dose of Roses®, is that you (or another recipient) falls in love with your gorgeous arrangement of Timeless Roses. We offer an extensive selection of different styles and colors, allowing you to find the perfect arrangement for anyone on your list. If you have any questions about our return policy or our arrangements, please don’t hesitate to contact us!'
                 }
             ]
@@ -70,6 +80,7 @@ export const faqPageData = {
             question: 'How long does it take for my order to ship?',
             answer: [
                 {
+                    type: 'p',
                     content: 'Please allow 1-3 business days for us to process and ship your order. Orders placed after 1pm will be processed the next business day. All orders are shipped FedEx or USPS and International orders are shipped USPS International under this shipping method chosen at checkout.'
                 }
             ]
@@ -78,13 +89,41 @@ export const faqPageData = {
             question: 'Does Dose of Roses deliver?',
             answer: [
                 {
+                    type: 'p',
                     content: 'Dose of Roses does offer hand delivery via a courier service (Roadie) for the Los Angeles Area as well as local pick/up from our Los Angeles warehouse. All orders must be placed before 1pm PST to be delivered the same day.'
                 },
                 {
-                    content: 'Local Pick-Up ● Pick Up Time (10am-3:30pm) ● Customers will be notified via email when the package is available for pick up. ● Customers must provide Order Number and valid ID when coming to pick up at our facility.'
+                    type: 'u',
+                    title: 'Local Pick-Up',
+                    content: [
+                        {
+                            item: 'Pick Up Time (10am-3:30pm)'
+                        },
+                        {
+                            item: 'Customers will be notified via email when the package is available for pick up.'
+                        },
+                        {
+                            item: 'Customers must provide Order Number and valid ID when coming to pick up at our facility.'
+                        }
+                    ]
                 },
                 {
-                    content: 'Local Delivery ● 40 mile Radius ● $35 Flat rate charge (subject to change) ● Delivery Timeframe (ASAP or specified time) ● Info via text only - customer will receive text message when the order is on the way!'
+                    type: 'u',
+                    title: 'Local Delivery',
+                    content: [
+                        {
+                            item: '40 mile Radius'
+                        },
+                        {
+                            item: '$35 Flat rate charge (subject to change)'
+                        },
+                        {
+                            item: 'Delivery Timeframe (ASAP or specified time)'
+                        },
+                        {
+                            item: 'Info via text only - customer will receive text message when the order is on the way!'
+                        }
+                    ]
                 }
             ]
         },
@@ -92,102 +131,150 @@ export const faqPageData = {
             question: 'Can I purchase my item on site, for Local Pick Up?',
             answer: [
                 {
+                    type: 'p',
                     content: 'All sales must be made online prior to picking up your order at our warehouse. We do not offer in house purchases or exchanges.'
                 },
                 {
-                    content: 'Shipping Timeframes: FedEx Ground / Home Delivery,1-5 business days'
+                    type: 'strong',
+                    style: 'text-decoration: underline',
+                    content: 'Shipping Timeframes'
+                },
+
+                {
+                    type: 'strong',
+                    content: 'FedEx Ground / Home Delivery'
                 },
                 {
-                    content: 'FedEx 2Day: 2 business days'
+                    type: 'p',
+                    content: '1-5 business days'
+                },
+
+                {
+                    type: 'strong',
+                    content: 'FedEx 2Day'
                 },
                 {
-                    content: 'FedEx Standard Overnight: 1 business day'
+                    type: 'p',
+                    content: '2 business days'
+                },
+
+                {
+                    type: 'strong',
+                    content: 'FedEx Standard Overnight'
                 },
                 {
-                    content: 'USPS Priority Mail: 1-3 days (Mon - Sat)'
+                    type: 'p',
+                    content: '1 business day'
+                },
+
+                {
+                    type: 'strong',
+                    content: 'USPS Priority Mail'
                 },
                 {
-                    content: 'USPS Priority Mail Express: 1-2 days (Mon - Sat)'
-                }                
+                    type: 'p',
+                    content: '1-3 days (Mon - Sat)'
+                },
+
+                {
+                    type: 'strong',
+                    content: 'USPS Priority Mail Express'
+                },
+                {
+                    type: 'p',
+                    content: '1-2 days (Mon - Sat)'
+                },
             ]
         },
         {
-            question: '',
+            question: 'What is your shipping policy?',
             answer: [
                 {
-                    content: ''
+                    type: 'p',
+                    content: 'At Dose of Roses®, shipping cost is determined by the item purchased and the delivery address. If your roses are being hand-delivered by our courier service, then your arrangement will be delivered the same day they are ordered. Deliveries throughout the U.S. will ship through FedEx or USPS depending on where you live.'
+                },
+                {
+                    type: 'p',
+                    content: 'Do you want to send Dose of Roses® to a friend or loved one who is overseas? We also offer shipping to more than 50 countries throughout the world! With that in mind, most international orders may take additional time for processing or identity verification for orders that have been flagged. If there are complications with your order, then our team will send you a notification to alert you of the delay.'
+                },
+                {
+                    type: 'p',
+                    content: 'To learn more about our international deliveries, check out our <a href="/pages/shipping">shipping page.</a>',
                 }
             ]
         },
         {
-            question: '',
+            question: 'Can I change my shipping address?',
             answer: [
                 {
-                    content: ''
+                    type: 'p',
+                    content: 'Shipping addresses can be changed <strong>ONLY</strong> if the order has not been fulfilled and is not guaranteed. If the order has been fulfilled we advise reaching out to the shipping carrier for an alternate solution.'
                 }
             ]
         },
         {
-            question: '',
+            question: 'My Package was delivered but I did not receive it, what should I do ?',
             answer: [
                 {
-                    content: ''
+                    type: 'p',
+                    content: 'If you have received notification that your order has been delivered, but you are unable to locate it, we ask that you wait 24-48 hoursbefore reaching out. It is likely the courier service may have scanned the package as “delivered” during the transfer of the package from their facility to their trucks.'
+                },
+                {
+                    type: 'p',
+                    content: 'If still not located, please check your order confirmation and confirm if you purchased Route Shipping Protection. If so, please submit a claim with Route for the missing package. <a href="If%20still%20not%20located,%20please%20check%20your%20order%20confirmation%20and%20confirm%20if%20you%20purchased%20Route%20Shipping%20Protection.%20If%20so,%20please%20submit%20a%20claim%20with%20Route%20for%20the%20missing%20package.%20https://claims.route.com/">https://claims.route.com/</a>'
+                },
+                {
+                    type: 'p',
+                    content: 'If you did not purchase the Route Shipping Protection please reach out to our customer care for further assistance. <a href="mailto:support@doseofroses.com" aria-describedby="a11y-external-message">support@doseofroses.com</a>'
                 }
             ]
         },
         {
-            question: '',
+            question: 'What is Route - Shipping Insurance/Protection?',
             answer: [
                 {
-                    content: ''
+                    type: 'p',
+                    content: 'Route Shipping Protection provides transparency for customers post purchase experience. Route also insures you against lost, stolen or damaged packages. Please reach out to Route to submit a claim (<a href="https://claims.route.com/" aria-describedby="a11y-external-message">https://claims.route.com/</a>).You will receive a "Welcome Email" from Route via the email address provided at checkout.'
                 }
             ]
         },
         {
-            question: '',
+            question: 'My package was returned to the sender (RTS), what should I do?',
             answer: [
                 {
-                    content: ''
+                    type: 'p',
+                    content: 'If a package is returned to our warehouse, Dose of Roses will provide the customer with a refund* in the amount of the order subtotal minus any discounts. We do not include the shipping cost as it was paid to ship the order.'
                 }
             ]
         },
         {
-            question: '',
+            question: 'What is Rush Processing? And how does it work?',
             answer: [
                 {
-                    content: ''
+                    type: 'p',
+                    content: 'Rush Processing expedites the fulfillment of your order so it can be verified, tailored to our Dose of Roses standards, quality checked, packaged and on the road within 24 hrs. For Rush Processing to be effective, your order must be submitted and charged before (12pm) local time (PST) Mon-Fri. If the above conditions are met and we fail to ship your order within 24 hrs, we will refund the Rush Processing fee.'
+                },
+                {
+                    type: 'p',
+                    content: 'Rush Processing fee <strong>DOES NOT</strong> affect the actual shipping time in transit. If you need your order quickly, please be sure to choose the appropriate shipping method as well.'
                 }
             ]
         },
         {
-            question: '',
+            question: 'My order was flagged as fraud?',
             answer: [
                 {
-                    content: ''
+                    type: 'p',
+                    content: 'At times an order may be held by our fraud prevention team for further review. You will receive an email as well as a phone call from 1 of our Customer Care Agents if your order is flagged. Please provide the required documents to <a href="mailto:support@doseofroses.com" aria-describedby="a11y-external-message">support@doseofroses.com</a>. We apologize for any inconvenience this may cause. Please note you will see a pending charge on your statement. Customer privacy and security is a top priority for us.'
                 }
             ]
         },
         {
-            question: '',
+            question: 'Can I create a custom order?',
             answer: [
                 {
-                    content: ''
-                }
-            ]
-        },
-        {
-            question: '',
-            answer: [
-                {
-                    content: ''
-                }
-            ]
-        },
-        {
-            question: '',
-            answer: [
-                {
-                    content: ''
+                    type: 'p',
+                    content: 'Yes, at Dose of Roses you do have the option to make a custom arrangement. To learn more about the Dose Customizer, check out our <a href="/pages/create">Create your Rose Box</a> page.'
                 }
             ]
         },
--- src/pages/pages/faqs.js
+++ src/pages/pages/faqs.js
@@ -4,14 +4,19 @@ import { faqPageData } from '../../data/faq'
 
 const Faqs = ({ data }) => {      
     useEffect(() => {
-        /*
-        document.querySelectorAll('.faq_content-accordion_button').toArray.forEach(button => {
+        document.querySelectorAll('.accordion_button').forEach(button => {
             const accordionButton = button;
+            accordionButton.innerHTML = accordionButton.innerHTML + '<i className="fas fa-angle-down"></i>';
+            button.addEventListener('click', () => {
+                button.classList.toggle('accordion_button--active');
+            });
+        });
+
+        document.querySelectorAll('.faq_content-accordion_button').forEach(button => {
             button.addEventListener('click', () => {
                 button.classList.toggle('faq_content-accordion_button--active');
             });
         });
-        */
     })
       
     return (
@@ -34,15 +39,19 @@ const Faqs = ({ data }) => {
             <div id="shopify-section-faq-content" className="shopify-section">
                 <div className="faq_content-container">
                     <div className="faq_content-wrapper">
-                        <button className="faq_content-accordion_button">
-                            Where did Dose of Roses come from?
-                            <img src="//cdn.shopify.com/s/files/1/0157/4420/4900/t/230/assets/arrow_down_white_200x.png?v=5587476236961067168" alt="" />
-                        </button>
-                        <div className="faq_content-accordion_content">
-                            <p>The brand was founded by Joseph Ayoub, founder of some of the most well-known digital brands, and Julian Wilson, a former executive of the #1 most-searched fashion brand. Founded on January 6, 2019, Joseph and Julian were looking to start a new brand together and Valentine’s Day was right around the corner. Roses came to mind as they are the symbol of love and what most people give to their loved ones. Being different from the rest, they began selling this unique gift called the Rose Bear, a bear covered in synthetic roses that last a lifetime. After selling out completely in a matter of weeks, they started roaming through the stores of Los Angeles to find Rose Bears but weren’t able to find it as it was a relatively new product that wasn’t available in stores. Having no luck, they started looking for other gifts and that’s where they came across preserved rose arrangements. They had bought the entire inventory of multiple stores and had sold out of those in a matter of days. This is where the idea of gifting a unique luxury experience that isn’t the traditional bouquet of roses that only last a few days was born.</p>
-                            <br />
-                            <p>Today, you can create hundreds of different arrangements featuring unique shapes, colors, and sizes. Each arrangement contains anywhere from one to more than 150 Timeless Roses, and with proper care, your arrangement can last up to five years</p>
-                        </div>
+                        { faqPageData.faqContent.map((item, index) => 
+                            <>
+                            <button className="faq_content-accordion_button" key={`btn_${index}`}>
+                                {item.question}
+                                <img src="//cdn.shopify.com/s/files/1/0157/4420/4900/t/230/assets/arrow_down_white_200x.png?v=5587476236961067168" alt="" />
+                            </button>
+                            {/* <div className="faq_content-accordion_content" key={`div_${index}`}>
+                                {item.answer.map((answer_item, answer_index) => 
+                                    <p key={`answer_${answer_index}`}>{answer_item}</p> 
+                             )}
+                            </div> */}
+                            </>
+                        )}
                     </div>
                 </div>
             </div>
