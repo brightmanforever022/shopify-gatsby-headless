3931e73d0c3968b694265ded9b7430c71a28cad0
brightmanforever022 <brightmanforever@outlook.com>
2021-02-03 08:13:37 -0800

    slick to flickity

--- src/components/collectionPage/productBoxGallery.js
+++ src/components/collectionPage/productBoxGallery.js
@@ -1,10 +1,13 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 // import { LazyLoadImage } from 'react-lazy-load-image-component'
 import CustomImage from '../common/image'
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import '../../styles/productGallery.css';
+
+import Flickity from 'react-flickity-component'
+import '../../styles/flickity.css';
 
 const ProductBoxGallery = props => {
     let productGalleryCount = 0;
@@ -25,7 +28,19 @@ const ProductBoxGallery = props => {
     }
     const [ swatchImages, setSwatchImages ] = useState([]);
 
+    const flickityOptions = {
+        pageDots: false, 
+        imagesLoaded: true, 
+        touchVerticalScroll: false,
+        dragThreshold : 10, 
+        selectedAttraction: 0.01,
+        friction: 0.2,
+        beforeChange: (current, next) => setSlideIndex(next)
+    }
+    
     productGalleryCount = mainOption === '' ? Math.min(product.images.length,3) : Math.min(swatchImages.length,3);
+
+
 
     useEffect(() => {
         if (mainOption !== '') {
@@ -42,6 +57,14 @@ const ProductBoxGallery = props => {
             const filteredImages = selectedImages.filter(img => img !== '')
             setSwatchImages(filteredImages.slice(0, 3))
         }
+
+        if (flkty) {
+            flkty.on('change', () => {
+                setSlideIndex(flkty.selectedIndex);
+                console.log("flkty.selectedIndex = ", flkty.selectedIndex)
+            })
+        }
+
     }, [mainOption, swatchColor, product.variants])
 
     const isBadgeEnable = () => {
@@ -89,13 +112,65 @@ const ProductBoxGallery = props => {
         }
     }
 
+    let slider = useRef(null);
+
+    let flkty = null;
+
     return (
         <div className="product-card__image-with-placeholder-wrapper" data-image-with-placeholder-wrapper>
             <div className="grid-view-item__image-wrapper product-card__image-wrapper js">
                 {   
                     isBadgeEnable() ? <img  src={getBadgeImage()} className="badge" alt="" /> : ''
                 }
+
                 <div className="collection-product_image_container">
+                {
+                    mainOption === '' ?
+                        <Flickity options={flickityOptions} flickityRef={c=> flkty = c} >
+                            { product.images[0] ? 
+                                (<CustomImage 
+                                    className="product-tile__image product-collection_image_primary grid-view-item__image lazy-load-mc"
+                                    src={product.images[0].originalSrc}
+                                    alt={product.title}
+                                />) : ""
+                            }
+                            { product.images[1] ? 
+                                (<CustomImage 
+                                    className="product-tile__image product-collection_image_primary grid-view-item__image lazy-load-mc"
+                                    src={product.images[1].originalSrc}
+                                    alt={product.title}
+                                    style={{ cursor: 'pointer' }} 
+                                />) : ""
+                            }
+                            { product.images[2] ? 
+                                (<CustomImage 
+                                    className="product-tile__image product-collection_image_primary grid-view-item__image lazy-load-mc"
+                                    src={product.images[2].originalSrc}
+                                    alt={product.title}
+                                    style={{ cursor: 'pointer' }} 
+                                />) : ""
+                            }
+                        </Flickity>
+                    : 
+                    <Flickity options={flickityOptions}>
+                    {
+                        swatchImages.map((swatchImage, swatchImageIndex) => {
+                            return (
+                                <CustomImage 
+                                    className="product-tile__image product-collection_image_primary grid-view-item__image lazy-load-mc"
+                                    src={swatchImage}
+                                    alt=''
+                                    style={{ cursor: 'pointer' }} 
+                                    key={swatchImageIndex}
+                                />
+                            )
+                        })
+                    }
+                    </Flickity>
+                }
+                </div>
+
+                {/* <div className="collection-product_image_container">
                 { 
                     mainOption === '' ?
                         <Slider {...productBoxSliderSettings}>
@@ -140,7 +215,7 @@ const ProductBoxGallery = props => {
                     }
                     </Slider>
                 }
-                </div>
+                </div> */}
             </div>
                     
             <div className="carousel-scrollbar">
