3ac4501704d54cbd46ea1f88facd0f5f114779e0
brightmanforever022 <brightmanforever@outlook.com>
2021-02-05 00:38:55 -0800

    bug fixed

--- src/templates/productPage.js
+++ src/templates/productPage.js
@@ -94,13 +94,29 @@ export const query = graphql`
                 id
                 handle
                 title
+                tags
+                options {
+                    id
+                    name
+                    values
+                }
                 images {
                     originalSrc
                 }
                 variants {
                     id
+                    title
                     availableForSale
                     price
+                    shopifyId
+                    compareAtPrice
+                    image {
+                        originalSrc
+                    }
+                    selectedOptions {
+                        name
+                        value
+                    }
                 }
             }
         }
