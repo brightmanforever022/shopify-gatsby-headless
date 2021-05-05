b56c23899aa8bf09def16eb5cdb17118d3d54f63
brightmanforever022 <brightmanforever@outlook.com>
2021-05-05 16:59:31 +0700

    Done for updating UI of Shipping Protection with modal included

--- src/components/common/orderProtection.js
+++ src/components/common/orderProtection.js
@@ -1,46 +1,60 @@
 import React, { useState } from 'react';
-import Switch from "react-switch";
+// import Switch from 'react-switch';
 import '../../styles/order-protection.scss';
+import loadable from '@loadable/component';
+const OrderProtectionInfo = loadable(() => import('./orderProtectionInfo'));
 
 const OrderProtection = (props) => {
   const [checked, setChecked] = useState(true);
+  const [openOrderProtectionInfo, setOpenOrderProtectionInfo] = useState(false);
   const protectionVariant = props.protectionVariant;
   const context = props.context;
   const lineItems = props.lineItems;
-  const filteredItems = lineItems.filter(item => item.variant.id === protectionVariant.shopifyId)
+  const filteredItems = lineItems.filter((item) => item.variant.id === protectionVariant.shopifyId);
   let protectionLineItem = null;
-  if(filteredItems.length > 0) {
+  if (filteredItems.length > 0) {
     protectionLineItem = filteredItems[0];
   }
 
-  
   const removeProtection = (itemId) => {
-		context.removeLineItem(context.store.client, context.store.checkout.id, itemId)
-	}
+    context.removeLineItem(context.store.client, context.store.checkout.id, itemId);
+  };
   const addProtection = (itemId) => {
-    context.addVariantToCart(itemId, 1)
-  }
+    context.addVariantToCart(itemId, 1);
+  };
   const handleChange = (checked) => {
-    if(checked) {
-      addProtection(protectionVariant.shopifyId)
+    if (checked) {
+      addProtection(protectionVariant.shopifyId);
     } else {
-      removeProtection(protectionLineItem.id)
+      removeProtection(protectionLineItem.id);
     }
     setChecked(checked);
   };
   return (
     <div className="order-protection">
       <div className="order-protection-mark">
-        <img src="https://cdn.shopify.com/s/files/1/0157/4420/4900/products/2IBrfX8_350x350.png?v=1619630339" alt="" />
-        ORDERPROTECTION.COM
-        Shipping Protection
-        from Damage, Loss, & Theft for $2.15
-        By deselecting shipping protection, Dose of Roses is not liable for lost, damaged, or stolen items.
+        <div className="order-protection-mark__logo">
+          <img src="https://cdn.shopify.com/s/files/1/0157/4420/4900/products/2IBrfX8_350x350.png?v=1619630339" alt="" />
+        </div>
+        <span>
+          ORDER<strong>PROTECTION.COM</strong>
+        </span>
+        <div className="order-protection-mark__info" onClick={() => setOpenOrderProtectionInfo(true)}>
+          <div className="info">i</div>
+        </div>
       </div>
-      <Switch onChange={(val)=>handleChange(val)} checked={checked} />
+      <div className="order-protection-price">$2.15</div>
+      <div className="switcher">
+        <label class="switch">
+          <input type="checkbox" onChange={(e) => handleChange(e.target.checked)} checked={checked} />
+          <span class="slider rounded"></span>
+        </label>
+      </div>
+      {openOrderProtectionInfo && <OrderProtectionInfo closeModal={setOpenOrderProtectionInfo}/>}
+      {/* <Switch onChange={(val) => handleChange(val)} checked={checked} /> */}
     </div>
   );
 };
 
-OrderProtection.displayName = "OrderProtection";
+OrderProtection.displayName = 'OrderProtection';
 export default OrderProtection;
